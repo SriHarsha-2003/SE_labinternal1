807713779ff7870958ef4b672850c4f5e1138eb9
sri harsha <sriharshasirivolu@gmail.com>
2022-11-23 11:40:32 +0530

    labinternal

---
+++ src/main/java/com/app/maven/ExamWeb/App.java
@@ -0,0 +1,13 @@
+package com.app.maven.ExamWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Se lab internal 1 exam ,the created maven project is ExamWeb" );
+    }
+}
